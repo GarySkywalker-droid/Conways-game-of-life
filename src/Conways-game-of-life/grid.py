14b3e666c59201c5883daa26a9671fe32f18af55
GarySkywalker-droid <194703971+GarySkywalker-droid@users.noreply.github.com>
2025-06-28 15:36:31 -0400

    feat: added patterns object to represent the game pattern and the grid for the finding neighbors and applying the game rules

---
+++ src/Conways-game-of-life/grid.py
@@ -0,0 +1,37 @@
+import collections
+from patterns import Pattern
+
+
+class LifeGrid:
+    def __init__(self, pattern: Pattern) -> None:
+        self.pattern = pattern
+
+    def evolve(self) -> None:
+        neighbors = [
+            (-1, -1),
+            (-1, 0),
+            (0, -1),
+            (0, 1),
+            (1, 0),
+            (-1, 1),
+            (1, -1),
+            (1, 1),
+        ]
+        num_neighbors = collections.defaultdict(int)
+        for row, col in self.pattern.alive_cells:
+            for drow, dcol in neighbors:
+                num_neighbors[(row + drow, col + dcol)] += 1
+
+        stay_live = {
+            cell for cell, num in num_neighbors.items() if num in {2, 3}
+        } & self.pattern.alive_cells
+        new_live = {
+            cell for cell, num in num_neighbors.items() if num == 3
+        } - self.pattern.alive_cells
+
+        self.pattern.alive_cells = stay_live | new_live
+
+    def __str__(self) -> str:
+        return (
+            f"{self.pattern.name}:\nAlive cells -> {sorted(self.pattern.alive_cells)}"
+        )
